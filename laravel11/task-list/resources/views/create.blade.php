4e8ea2e6be42e0629addae8b2172f0340575aa95
jery123 <85230959+jery123@users.noreply.github.com>
2025-09-18 11:58:52 +0530

    Reussing blade

--- laravel11/task-list/resources/views/create.blade.php
+++ laravel11/task-list/resources/views/create.blade.php
@@ -1,53 +1,5 @@
 @extends('layouts.app')
 
-@section('title', "Add Task")
-
-@section('style')
-    <style>
-        .error-message{
-           color:red;
-           font-size: 0, 8rem; 
-        }
-    </style>
-@endsection
-
 @section('content')
-
-<form action="{{ route('tasks.store') }}" method="POST">
-    @csrf
-
-    <div>
-        <label for="title">Title</label>
-        <input name="title" id="title" value="{{ old('title') }}">
-        @error('title')
-            <p class="error-message">{{ $message }}</p>
-        @enderror
-    </div>
-        
-    <div>
-        <label for="description">Description</label>
-        <textarea name="description" id="description" rows="5">
-            {{ old('description') }}
-        </textarea>
-        @error('description')
-            <p class="error-message">{{ $message }}</p>
-        @enderror
-    </div>
-
-    <div>
-        <label for="long_description">Long Description</label>
-        <textarea name="long_description" id="long_description" rows="5">
-            {{ old('long_description') }}
-        </textarea>
-        @error('long_description')
-            <p class="error-message">{{ $message }}</p>
-        @enderror
-    </div>
-        
-    <div>
-        <input type="submit" value="Add Task">
-    </div>
-
-    </div>
-</form>
+    @include('form')
 @endsection
--- laravel11/task-list/resources/views/edit.blade.php
+++ laravel11/task-list/resources/views/edit.blade.php
@@ -1,53 +1,5 @@
 @extends('layouts.app')
 
-@section('title', "Edit Task")
-
-@section('style')
-    <style>
-        .error-message{
-           color:red;
-           font-size: 0, 8rem;
-        }
-    </style>
-@endsection
-
 @section('content')
-
-<form action="{{ route('tasks.update', ['task'=>$task->id]) }}" method="POST">
-    @csrf
-    @method('PUT')
-    <div>
-        <label for="title">Title</label>
-        <input name="title" id="title" value="{{ $task->title }}">
-        @error('title')
-            <p class="error-message">{{ $message }}</p>
-        @enderror
-    </div>
-
-    <div>
-        <label for="description">Description</label>
-        <textarea name="description" id="description">
-            {{ $task->description }}
-        </textarea>
-        @error('description')
-            <p class="error-message">{{ $message }}</p>
-        @enderror
-    </div>
-
-    <div>
-        <label for="long_description">Long Description</label>
-        <textarea name="long_description" id="long_description">
-            {{ $task->long_description }}
-        </textarea>
-        @error('long_description')
-            <p class="error-message">{{ $message }}</p>
-        @enderror
-    </div>
-
-    <div>
-        <input type="submit" value="Edit Task">
-    </div>
-
-    </div>
-</form>
+ @include('form', ['tash' => $task])
 @endsection
