be105ac1c5405aeee56de28c102dd25f63f60cd9
jery123 <85230959+jery123@users.noreply.github.com>
2025-09-18 10:36:32 +0530

    Keeping old route values

--- laravel11/task-list/resources/views/layouts/app.blade.php
+++ laravel11/task-list/resources/views/layouts/app.blade.php
@@ -5,10 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @yield('style')
 </head>
 <body>
     <h1>@yield('title')</h1>
     <div>
+        @if (session()->has('success'))
+            <div>
+                {{ session('success') }}
+            </div>
+        @endif
         @yield('content')
     </div>
 </body>
