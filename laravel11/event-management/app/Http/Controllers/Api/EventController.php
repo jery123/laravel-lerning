ff3754c7fbaa2834299d6cf58fa9c75753de40e8
jery123 <85230959+jery123@users.noreply.github.com>
2025-09-22 16:32:32 +0530

    Loading optional relationship

--- laravel11/event-management/app/Http/Controllers/Api/EventController.php
+++ laravel11/event-management/app/Http/Controllers/Api/EventController.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+    use CanLoadRelationships;
+
+    private array $relations = ['user', 'attendees', 'attendees.user'];
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
+        $query = $this->loadRelationships(Event::query());
+
         return EventResource::collection(
-                Event::with('user')->paginate()
+                $query->latest()->paginate()
             );
     }
 
@@ -34,7 +41,7 @@ class EventController extends Controller
             'user_id' => 1,
        ] );
 
-        return new EventResource($event);
+        return new EventResource($this->loadRelationships($event));
 
     }
 
