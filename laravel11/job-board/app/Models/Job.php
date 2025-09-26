d3cdcd63472419851108b7d33e41c9efe1ae5b11
jery123 <85230959+jery123@users.noreply.github.com>
2025-09-26 16:31:16 +0530

    Auth logic

--- laravel11/job-board/app/Models/Job.php
+++ laravel11/job-board/app/Models/Job.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Eloquent\Builder;
 
 class Job extends Model
 {
@@ -19,4 +21,34 @@ class Job extends Model
                 'Sales',
                 'Marketing'
     ];
+
+
+    public function employer(){
+        return $this->belongsTo(Employer::class);
+    }
+
+
+    public function scopeFilter(Builder | QueryBuilder $query, array $filters): Builder | QueryBuilder{
+        return $query->when($filters['search'] ?? null, function ($query, $search) {
+                $query->where(function ($query) use ($search) {
+                    $query->where('title', 'like', '%'.$search.'%')
+                          ->orWhere('description', 'like', '%'.$search.'%')
+                          ->orWhereHas('employer', function($query) use ($search){
+                                $query->where('company_name', 'like', '%' . $search . '%');
+                          });
+                });
+            })
+            ->when($filters['min_salary'] ?? null, function ($query, $minSalary) {
+                $query->where('salary', '>=', $minSalary);
+            })
+            ->when($filters['max_salary'] ?? null, function ($query, $maxSalary) {
+                $query->where('salary', '<=', $maxSalary);
+            })
+            ->when($filters['experience'] ?? null, function($query, $experience){
+                $query->where('experience', $experience);
+            })
+            ->when($filters['category'] ?? null, function($query, $category){
+                $query->where('category', $category);
+            });
+    }
 }
---
+++ laravel11/job-board/database/migrations/2025_09_26_062753_create_employers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\Employer;
+use App\Models\Job;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employers', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('company_name');
+            $table->foreignIdFor(User::class)
+                    ->nullable()
+                    ->constrained();
+
+            $table->timestamps();
+        });
+
+        Schema::table('table_jobs', function(Blueprint $table){
+            $table->foreignIdFor(Employer::class)->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('table_jobs', function(Blueprint $table){
+            $table->dropForeignIdFor(Employer::class);
+        });
+
+        Schema::dropIfExists('employers');
+    }
+};
