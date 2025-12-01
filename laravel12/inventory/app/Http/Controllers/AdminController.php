e4d280cb5647feacab0757113d6366b45d7b2ee7
jery123 <85230959+jery123@users.noreply.github.com>
2025-12-01 18:32:42 +0530

    completing with the admin profile And image update and added toastr message

--- laravel12/inventory/app/Http/Controllers/AdminController.php
+++ laravel12/inventory/app/Http/Controllers/AdminController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -20,10 +19,53 @@ class AdminController extends Controller
         return redirect('/login');
     }
 
-    public function AdminProfile(){
+    public function AdminProfile()
+    {
         $id = Auth::user()->id;
         $profileData = User::find($id);
 
         return view('admin.admin_profile', compact('profileData'));
+    }
+
+    public function ProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        $oldPhoto = $data->phot;
+
+        if ($request->hasFile(('photo'))) {
+            $file = $request->file('photo');
+            $filename = time().'.'.$file->getClientOriginalExtension();
+            $file->move(public_path('upload/user_images'), $filename);
+            $data->photo = $filename;
+
+            if ($oldPhoto && $oldPhoto != $filename) {
+                $this->deleteOldPhoto($oldPhoto);
+
+            }
+        }
+
+        $data->save();
+
+        $notification = array(
+            'message' => 'Profile Updated Successfully',
+            'alert-type' => 'success',
+        );
+
+        return redirect()->back()->with($notification);
+    }
+
+    private function deleteOldPhoto(string $oldPhoto)
+    {
+        $oldPhotoPath = public_path('upload/user_images/'.$oldPhoto);
+        if (file_exists($oldPhotoPath)) {
+            unlink($oldPhotoPath);
+        }
     }
 }
